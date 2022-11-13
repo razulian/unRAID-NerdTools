69a1de0c1f49d81b63458200f154a39d086cf613
razulian <info@ilhanunal.com>
2022-11-13 09:16:42 +0100

    Bumped version + added package kernel-headers-5.19.17-x86-1.txz

--- source/NerdTools/usr/local/emhttp/plugins/NerdTools/include/NerdToolsHelpers.php
+++ source/NerdTools/usr/local/emhttp/plugins/NerdTools/include/NerdToolsHelpers.php
@@ -13,7 +13,7 @@ if (!is_dir($pkg_extra_path))
 
 $pkg_desc = 'https://raw.githubusercontent.com/razulian/unRAID-NerdTools/main/packages/packages-desc';
 $pkg_repo = "https://api.github.com/repos/razulian/unRAID-NerdTools/contents/packages/$os_version";
-$pkg_repo = $_SERVER['HTTP_DEVELOP'] ? $pkg_repo = "https://api.github.com/repos/jsavargas/unRAID-NerdTools/contents/packages/$os_version?ref=develop" : $pkg_repo;
+$pkg_repo = $_SERVER['HTTP_DEVELOP'] ? $pkg_repo = "https://api.github.com/repos/razulian/unRAID-NerdTools/contents/packages/$os_version?ref=develop" : $pkg_repo;
 
 $desc_file   = $pkg_path.'packages-desc';
 $repo_file   = $pkg_path.'packages.json';
